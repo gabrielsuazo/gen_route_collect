a12c8802cd60ea7190b33ec457be5c265d1d7e73
gabri <gabrielsuazo7@gmail.com>
2025-02-05 13:47:35 +0100

    [DEV] Defined color parameters for displays

--- source/params.py
+++ source/params.py
@@ -2,3 +2,6 @@ SIZE_X = 200
 SIZE_Y = 200
 CLIENT_VOLUME_RANGE = [3, 9]
 MAXIMUM_VOLUME = 30
+STARTING_POINT_COLOR = "blueviolet"
+CLIENTS_COLORS = ("springgreen", "gold", "r")
+ROUTE_COLORS = ("orange", "olivedrab", "royalblue", "orchid", "firebrick", "darkgoldenrod", "palegreen", "turquoise")
